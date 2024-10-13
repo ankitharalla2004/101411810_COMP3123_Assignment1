54995857606b08c5a54abf580fc542bef87c49e9
RallaAnkitha09 <ankithar0107@gmail.com>
2024-10-13 18:56:22 -0400

    Assignment1

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouts = require("./routes/user.js");
+const empRouts = require("./routes/employee.js");
+
+const app = express();
+const DB_CONNECTION_STRING = "mongodb+srv://rallaankitha2004:5Q65q9ZLmVRm7lzd@cluster0.nnl44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("Error: ", err);
+});
+
+const SERVER_PORT = 8088;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Fixed typo here
+
+app.use("/api/v1/user", userRouts);
+app.use("/api/v1/emp", empRouts);
+
+app.route("/")
+    .get((req, res) => {
+        res.send("<h1>MogoDB + Mongoose Example</h1>");
+    });
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running at http://localhost:${SERVER_PORT}/`);
+});
